d9a14a15b7b5fb0ddbe96f7bde4a0264128b9bb4
jiby <jibyjohn7@gmail.com>
2014-08-18 11:34:57 +0530

    Proxy, facebook, api integrated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,9 +15,11 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngFacebook',
+    'LocalStorageModule',
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $facebookProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -27,7 +29,34 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+    $facebookProvider.setAppId('273647349502832');
+    $facebookProvider.setPermissions("email,user_likes,user_birthday,user_relationships,user_work_history,user_hometown,user_location,user_friends");
   });
+
+angular.module('sassApp').run( function() {
+	  // Load the facebook SDK asynchronously
+	  (function(){
+	     // If we've already installed the SDK, we're done
+	     if (document.getElementById('facebook-jssdk')) {return;}
+
+	     // Get the first script element, which we'll use to find the parent node
+	     var firstScriptElement = document.getElementsByTagName('script')[0];
+
+	     // Create a new script element and set its id
+	     var facebookJS = document.createElement('script'); 
+	     facebookJS.id = 'facebook-jssdk';
+
+	     // Set the new script's source to the source of the Facebook JS SDK
+	     facebookJS.src = '//connect.facebook.net/en_US/all.js';
+
+	     // Insert the Facebook JS SDK into the DOM
+	     firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+	   }());
+	});
---
+++ app/scripts/services/regservice.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name acslSoulcafeApp.regService
+ * @description
+ * # regService
+ * Factory in the acslSoulcafeApp.
+ */
+angular.module('sassApp')
+  .factory('regService', ['$http', '$facebook', function($http, $facebook) {
+
+	    var urlBase = '/Soulcafe/api/';
+	    var dataFactory = {};
+
+	    dataFactory.getChannelList = function () {
+	        return $http.get(urlBase);
+	    };
+	    dataFactory.getFbUserStatus = function (param) {
+	    	console.log(param);
+	    	var fbUrl = urlBase + 'users/' + param.id;
+	        return $http.get(fbUrl);
+	    };
+	    dataFactory.registerUser = function (param) {
+	    	//console.log(param);
+	    	var data = 'fb_id=' + param.id;
+	    	return $http.post(urlBase + 'add_user', param).then(function(response) {
+                return response;
+            });
+	    	
+	    };
+	    dataFactory.addEducationData = function (param) {
+	    	// console.log(param);
+	    	return $http.post(urlBase + 'add_education', param).then(function(response) {
+	    		console.log(response);
+                return response;
+            });
+	    	
+	    };
+	    dataFactory.addContactData = function (param) {
+	    	// console.log(param);
+	    	return $http.post(urlBase + 'add_contact', param).then(function(response) {
+	    		console.log(response);
+                return response;
+            });
+	    	
+	    };
+	    dataFactory.addCurrentPositionData = function (param) {
+	    	// console.log(param);
+	    	return $http.post(urlBase + 'add_currentposition', param).then(function(response) {
+	    		console.log(response);
+                return response;
+            });
+	    	
+	    };
+	    dataFactory.addPastPositionData = function (param) {
+	    	// console.log(param);
+	    	return $http.post(urlBase + 'add_pastposition', param).then(function(response) {
+	    		console.log(response);
+                return response;
+            });
+	    	
+	    };
+		dataFactory.getFbFriendsCount = function () {
+	    	return $facebook.api('/me/friends');
+	    };
+	    dataFactory.getUserDetails = function (uid) {
+	    	console.log(uid);
+	    	var fbUrl = urlBase + 'usersAll/' + uid;
+	        return $http.get(fbUrl);
+	    };
+	    return dataFactory;
+	}]);
