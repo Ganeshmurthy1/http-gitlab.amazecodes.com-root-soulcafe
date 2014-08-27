ae37c520e6f98ab180080401affd3a872f8a5274
Abhik <abhik@amazecoddes.com>
2014-08-27 14:48:49 +0530

    created discudssion topic page

--- app/scripts/controllers/discussion-list.js
+++ app/scripts/controllers/discussion-list.js
@@ -8,10 +8,30 @@
  * Controller of the sassApp
  */
 angular.module('sassApp')
-  .controller('DiscussionListCtrl', function ($scope) {
+  .controller('DiscussionListCtrl', function ($scope,localStorageService,regService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.userData = {};
+    function getUSerdata() {
+    	var authData = localStorageService.get('authorizationData');
+    	// console.log(authData);
+    	regService.getUserDetails(authData.user_id).then(function (results) {
+    		// console.log(results.data);
+    		$scope.userData = results.data; 
+    		console.log($scope.userData.birthdate);
+    		 var d1 = new Date($scope.userData.birthdate);
+  console.log($scope.userData.birthdate);
+    	var d2 = new Date();
+		var diff = d2.getFullYear()-d1.getFullYear();
+		console.log(diff);	
+    	});
+    
+      }
+     getUSerdata();
+    	
+
+ 
   });
