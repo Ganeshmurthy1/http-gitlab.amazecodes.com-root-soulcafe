7f5db771fd3fc42fc66ef6a8c2bd2abefb632b86
jiby <jibyjohn7@gmail.com>
2014-09-22 15:34:43 +0530

    Gruntfile local changes and db change

--- api/includes/dbconnection.php
+++ api/includes/dbconnection.php
@@ -4,7 +4,7 @@ function getConnection() {
 	$dbhost="localhost";
 	$dbuser="root";
 	$dbpass="123";
-	$dbname="soulcafe";
+	$dbname="soul-abhik";
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbh;
