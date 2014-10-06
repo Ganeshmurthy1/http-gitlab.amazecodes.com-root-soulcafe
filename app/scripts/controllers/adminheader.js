9ef6821f31248fc4adb5f43bc910c974f6531965
jiby <jibyjohn7@gmail.com>
2014-10-06 11:26:00 +0530

    Admin page checkuser function removed

--- app/scripts/controllers/adminheader.js
+++ app/scripts/controllers/adminheader.js
@@ -16,22 +16,22 @@ angular.module('sassApp')
     ];
     $scope.Header = 'views/admin-header.html';
     
-	$scope.home=function()
-	{
-		$location.path('/superadmin-profile');
-	}
-	$scope.user=function()
-	{
-		$location.path('/admin-userlist');
-	}
-	$scope.blocked=function()
-	{
-		$location.path('/admin-blocked-users');
-	}
-	
-	$scope.message=function()
-	{
-		$location.path('/superadmin-message');
-	}
+//	$scope.home=function()
+//	{
+//		$location.path('/superadmin-profile');
+//	}
+//	$scope.user=function()
+//	{
+//		$location.path('/admin-userlist');
+//	}
+//	$scope.blocked=function()
+//	{
+//		$location.path('/admin-blocked-users');
+//	}
+//	
+//	$scope.message=function()
+//	{
+//		$location.path('/superadmin-message');
+//	}
 	
   });
