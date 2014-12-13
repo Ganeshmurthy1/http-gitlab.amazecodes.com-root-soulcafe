74d35583dcce23d4eda3a4971a66b77b6add9203
jiby <jibyjohn7@gmail.com>
2014-12-13 12:58:27 +0530

    Word cloud changed

--- app/scripts/controllers/myprofile.js
+++ app/scripts/controllers/myprofile.js
@@ -14,22 +14,42 @@ angular.module('sassApp')
       'AngularJS',
       'Karma'
     ];
-    $facebook.api("/me/likes").then(function(pic) {
-    	console.log(pic.data);
-      if (pic.data == null) {
-        var likeData = [];
-      }else {
-        var likeData = [];
+    var likeData = [];
+    $facebook.api("/me/books").then(function(pic) {
+    	//console.log(pic.data);
+      if (pic.data != null) {
         for ( var int = 0; int < pic.data.length; int++) {
-          var tmpdata = {};
-          tmpdata.text = pic.data[int].name;
-          tmpdata.weight = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
-          likeData.push(tmpdata);
+	        	var tmpdata = {};
+	            tmpdata.text = pic.data[int].name;
+	            tmpdata.weight = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+	            likeData.push(tmpdata);
         }
-        $scope.words = likeData;
-        $scope.colors = ["#800026", "#bd0026", "#e31a1c", "#fc4e2a", "#fd8d3c", "#feb24c", "#fed976"];
       }
-    });
+    });   
+    $facebook.api("/me/movies").then(function(pic) {
+    	//console.log(pic.data);
+      if (pic.data != null) {
+        for ( var int = 0; int < pic.data.length; int++) {
+	        	var tmpdata = {};
+	            tmpdata.text = pic.data[int].name;
+	            tmpdata.weight = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+	            likeData.push(tmpdata);
+        }
+      }
+    });   
+    $facebook.api("/me/music").then(function(pic) {
+    	//console.log(pic.data);
+      if (pic.data != null) {
+        for ( var int = 0; int < pic.data.length; int++) {
+	        	var tmpdata = {};
+	            tmpdata.text = pic.data[int].name;
+	            tmpdata.weight = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+	            likeData.push(tmpdata);
+        }
+      }
+    });   
+        
+    $scope.words = likeData;
     
     $scope.thumbup = 'true';
     $scope.profileverify = '75%';
@@ -47,6 +67,6 @@ angular.module('sassApp')
 
 profileOperations.getMyProfileDetails().then(function(response) {
     console.log(response);    
-    $scope.interest=response.data;
+    $scope.interests = response.data;
   });
  }]);
