0e162d91977429a3c13039be2bab710595fb980a
Abhik <abhik@amazecoddes.com>
2015-01-19 12:20:03 +0530

    user side contact us done

--- app/scripts/controllers/contact-us.js
+++ app/scripts/controllers/contact-us.js
@@ -8,10 +8,22 @@
  * Controller of the sassApp
  */
 angular.module('sassApp')
-  .controller('ContactUsCtrl', function ($scope) {
+  .controller('ContactUsCtrl', function ($scope,regService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.cnctUs = function(){
+    	console.log($scope.cnct);
+    	regService.addContact($scope.cnct).then(function(response) {
+  		  if (response.data == 'true') {
+  			 console.log("Abhik");
+  		  }
+  		  else {
+  			  
+  		  }
+         });
+    }
   });
