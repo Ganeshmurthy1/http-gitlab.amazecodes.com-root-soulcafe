63692a93e500b82be384dc4e7642325f33b73e62
Abhik <abhik@amazecodes.com>
2015-01-29 13:45:32 +0530

    New images for slider added and close button change for why soulcafe

--- app/scripts/controllers/why-soul.js
+++ app/scripts/controllers/why-soul.js
@@ -16,7 +16,7 @@ angular.module('sassApp')
     ];
 
     
-$scope.images = [{src:'app-images/sliders/whysoulcafe-testslide1.jpg',no:0,hide:true},{src:'app-images/sliders/Slide2.JPG',no:1},{src:'app-images/sliders/Slide3.JPG',no:2},{src:'app-images/sliders/Slide4.JPG',no:3},{src:'app-images/sliders/Slide5.JPG',no:4},{src:'app-images/sliders/Slide6.JPG',no:5},{src:'app-images/sliders/Slide7.JPG',no:6	}];
+$scope.images = [{src:'app-images/sliders/Slide1.JPG',no:0,hide:true},{src:'app-images/sliders/Slide2.JPG',no:1},{src:'app-images/sliders/Slide3.JPG',no:2},{src:'app-images/sliders/Slide4.JPG',no:3},{src:'app-images/sliders/Slide5.JPG',no:4},{src:'app-images/sliders/Slide6.JPG',no:5},{src:'app-images/sliders/Slide7.JPG',no:6	}];
   
   });
 
