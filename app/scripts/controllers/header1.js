ff13bd51dc044673a06b2e9d5c7afcf5bfa17f30
Abhik <abhik@amazecodes.com>
2015-02-04 15:16:23 +0530

    Header and Footer seperated from static pages

--- app/scripts/controllers/header1.js
+++ app/scripts/controllers/header1.js
@@ -8,7 +8,7 @@
  * Controller of the sassApp
  */
 angular.module('sassApp')
-  .controller('Header1Ctrl', function ($scope, localStorageService,regService, $location) {
+  .controller('Header1Ctrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -18,53 +18,5 @@ angular.module('sassApp')
 
     $scope.Header1 = 'views/header1.html';
 
-    $scope.isAdmin = false;
-    var authData = localStorageService.get('authorizationData');
-    $scope.datau = authData;
-     var fbpic = localStorageService.get('fbpicture');
-     $scope.pic=fbpic.fbpicture.data.url;
-    if(authData.user_role == 1) {
-    	$scope.isAdmin = true;
-      $scope.isUser = true;
-    }
-    $scope.popup = false;
-
-
-
-    regService.getTotalSysMessage(authData.user_id).then(function(response) {
-          $scope.totalSysMessage = response.data.total; 
-      });
-      regService.getTotalMessage(authData.user_id).then(function(response) {
-          $scope.totalMessage = response.data.total; 
-      });
-      regService.getTotalForumMessage(authData.user_id).then(function(response) {
-          $scope.totalForumMessage = response.data.total; 
-      });
-
-      $scope.sysMessage = function() {  
-    	  regService.sysMarkMessage().then(function (response) { 
-    		  $scope.messages = response.data;
-    		  $scope.totalSysMessage = 0;
-    		  $scope.popup = true;
-      		
-      	});
-      };
-      
-      $scope.Message = function() {  
-    	  regService.MarkMessage().then(function (response) { 
-    		  $scope.messages = response.data;
-    		  $scope.totalMessage = 0;
-    		  $scope.popup = true;
-      		
-      	});
-      };
-      $scope.forumMessage = function() {  
-    	  regService.forumMarkMessage().then(function (response) { 
-    		  $scope.messages = response.data;
-    		  $scope.totalForumMessage = 0;
-    		  $scope.popup = true;
-      		
-      	});
-      };
-
+    
   });
